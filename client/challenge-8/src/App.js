0861b6a234667115c785bb5e60d0e13fa10736f9
bonasunu <bonaventurasunu@gmail.com>
2020-10-28 19:35:59 +0700

    add tailwind css

--- client/challenge-8/src/App.js
+++ client/challenge-8/src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './styles/main.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-3">
+      <h1 className="text-red-500">Challenge 8</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
