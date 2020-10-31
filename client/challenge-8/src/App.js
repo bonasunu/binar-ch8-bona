c0bd3c4a5033d52d9aa83a6f5ed7499931ddcdf9
bonasunu <bonaventurasunu@gmail.com>
2020-10-31 22:24:12 +0700

    implement json-server for mocking api

--- client/challenge-8/src/App.js
+++ client/challenge-8/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles/main.css'
+import Navbar from './components/Navbar'
+import playersService from './services/players'
 
 function App() {
+  const [players, setPlayers] = useState([])
+
+  useEffect(() => {
+    playersService
+      .getAllPlayers()
+      .then((response) => setPlayers(response))
+      .catch((e) => console.error(e.message))
+  }, [])
+
   return (
     <div className="m-3">
-      <h1 className="text-red-500">Challenge 8</h1>
+      <Navbar />
+      <h1 className="text-red-500">Game Dashboard</h1>
+      <p>Total players: {players.length}</p>
     </div>
   )
 }
---
+++ client/challenge-8/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <a>Menu</a>
+      <a>Menu</a>
+      <a>Menu</a>
+      <a>Menu</a>
+      <a>Menu</a>
+    </nav>
+  )
+}
+
+export default Navbar
