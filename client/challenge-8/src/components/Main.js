540c2a6e67da5b6c5b0773d3099d9575deb77f18
bonasunu <bonaventurasunu@gmail.com>
2020-11-02 13:02:08 +0700

    add react router

--- client/challenge-8/src/components/Main.js
+++ client/challenge-8/src/components/Main.js
@@ -2,12 +2,21 @@ import React from 'react'
 import Dashboard from './Dashboard'
 import CreatePlayerForm from './CreatePlayerForm'
 import FindPlayer from './FindPlayer'
+import { Switch, Route } from 'react-router-dom'
 
 const Main = ({ allPlayers, mainClass }) => (
   <div className={mainClass}>
-    <Dashboard getPlayers={allPlayers} />
-    <CreatePlayerForm />
-    <FindPlayer />
+    <Switch>
+      <Route path="/find">
+        <FindPlayer />
+      </Route>
+      <Route path="/create-player">
+        <CreatePlayerForm />
+      </Route>
+      <Route path="/">
+        <Dashboard getPlayers={allPlayers} />
+      </Route>
+    </Switch>
   </div>
 )
 
--- client/challenge-8/src/components/Navbar.js
+++ client/challenge-8/src/components/Navbar.js
@@ -1,27 +1,42 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = ({ navClass }) => {
   // styling
   const classes = {
     linkClass:
-      'w-full h-10 text-center hover:bg-gray-700 hover:bg-opacity-100 text-gray-500 hover:text-gray-200 m-1 p-1',
+      'w-full h-10 text-center hover:bg-gray-700 hover:bg-opacity-100 text-gray-500 hover:text-gray-200 visited:bg-red-200 m-1 p-1',
     logoClass:
       'h-16 mt-3 p-1 font-extrabold text-2xl text-gray-100 text-center w-full',
+    activeLink: 'bg-gray-700',
   }
 
   // render
   return (
     <nav className={navClass}>
       <p className={classes.logoClass}>Dashboard</p>
-      <a href="/home" className={classes.linkClass}>
+      <NavLink
+        className={classes.linkClass}
+        activeClassName={classes.activeLink}
+        exact
+        to="/"
+      >
         Dashboard
-      </a>
-      <a href="/home" className={classes.linkClass}>
+      </NavLink>
+      <NavLink
+        className={classes.linkClass}
+        activeClassName={classes.activeLink}
+        to="/create-player"
+      >
         Create Player
-      </a>
-      <a href="/home" className={classes.linkClass}>
-        Find players
-      </a>
+      </NavLink>
+      <NavLink
+        className={classes.linkClass}
+        activeClassName={classes.activeLink}
+        to="/find"
+      >
+        Find Player
+      </NavLink>
     </nav>
   )
 }
